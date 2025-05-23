a17554ca313e28124be131a4ca632d5e7f69e840
liljdot <jehuoki@gmail.com>
2025-05-23 18:52:29 +0100

    refactor PostsList component for consistent formatting

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -2,15 +2,15 @@ import { useGetPostsQuery } from "../state/api/postsSlice";
 import type { Post } from "../types";
 
 const PostsList: React.FC = () => {
-    const {data: posts} = useGetPostsQuery({})
+    const { data: posts } = useGetPostsQuery({})
 
     return (
         <>
-        <ul>
-            {posts?.map((post: Post) => <li key={post.id}>
-                {post.title}
-            </li>)}
-        </ul>
+            <ul>
+                {posts?.map((post: Post) => <li key={post.id}>
+                    {post.title}
+                </li>)}
+            </ul>
         </>
     )
 }
