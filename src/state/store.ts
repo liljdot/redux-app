f98342e6f6cd16c4643ee240329e625306c01b14
liljdot <jehuoki@gmail.com>
2025-05-23 18:25:22 +0100

    add posts slice and integrate with Redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/counterSlice"
+import { postsSlice } from "./api/postsSlice";
 
 export const store = configureStore({
     reducer: {
-        counter:  counterReducer
-    }
+        counter:  counterReducer,
+        [postsSlice.reducerPath]: postsSlice.reducer
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(postsSlice.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
