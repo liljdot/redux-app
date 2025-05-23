d249721f46ef5c230b4ad2c2f1c8f223cf58e7ba
liljdot <jehuoki@gmail.com>
2025-05-23 20:08:06 +0100

    add createPost mutation to postsSlice for post creation

--- src/state/api/postsSlice.ts
+++ src/state/api/postsSlice.ts
@@ -10,7 +10,14 @@ export const postsSlice = createApi({
         getPosts: builder.query<Post[], {limit: number, offset: number}>({
             query: ({limit,  offset}) => `/posts?_limit=${limit}&_offset=${offset}`,
         }), // 1st type argument is the response type, 2nd is the query parameters type. parameter supports only one object
+        createPost: builder.mutation<Post, Omit<Post, "id">>({
+            query: (newPost: Omit<Post, "id">) => ({
+                url: "/posts",
+                method: "POST",
+                body: newPost
+            })
+        })
     }),
 })
 
-export const { useGetPostsQuery } = postsSlice
+export const { useGetPostsQuery, useCreatePostMutation } = postsSlice
