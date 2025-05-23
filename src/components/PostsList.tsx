4455f561eeecd99a6022937c3eccaf9bc45f801b
liljdot <jehuoki@gmail.com>
2025-05-23 20:18:46 +0100

    add createPost functionality to PostsList component

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,11 +1,19 @@
-import { useGetPostsQuery } from "../state/api/postsSlice";
+import { useCreatePostMutation, useGetPostsQuery } from "../state/api/postsSlice";
 import type { Post } from "../types";
 
 const PostsList: React.FC = () => {
-    const { data: posts } = useGetPostsQuery({limit: 10, offset: 0})
+    const { data: posts, isLoading } = useGetPostsQuery({ limit: 10, offset: 0 })
+    const [createPostMutation, { isLoading: createPostIsLoading }] = useCreatePostMutation()
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
 
     return (
         <>
+        <button onClick={() => createPostMutation({title: "this is a new post"})}>
+            {createPostIsLoading ? "Creating..." : "Create Post"}
+        </button>
             <ul>
                 {posts?.map((post: Post) => <li key={post.id}>
                     {post.title}
